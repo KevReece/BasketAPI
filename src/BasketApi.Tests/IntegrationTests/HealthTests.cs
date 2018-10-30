2c32059dd0f4b1d950e27e5e541e602629870768
KevReece <kevinjamesreece@gmail.com>
2018-10-30 21:19:48 +0100

    Health controller for load balancer

---
+++ src/BasketApi.Tests/IntegrationTests/HealthTests.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BasketApi.Tests.IntegrationTests
+{
+    [TestClass]
+    public class HealthTests
+    {
+        private static TestClientFactory testClientFactory;
+
+        [ClassInitialize]
+        public static void BeforeAllTests(TestContext testContext)
+        {
+            testClientFactory = new TestClientFactory();
+        }
+
+        [ClassCleanup]
+        public static void AfterAllTests()
+        {
+            testClientFactory.Dispose();
+        }
+
+        [TestMethod]
+        public void GetsHealth()
+        {
+            var stringResponse = testClientFactory.Create().GetAsync("").Result.Content.ReadAsStringAsync().Result;
+
+            stringResponse.Should().Contain("Healthy");
+        }
+    }
+}
---
+++ src/BasketApi/Controllers/HealthController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BasketApi.Controllers
+{
+    [Route("")]
+    public class HealthController : Controller
+    {
+        [HttpGet]
+        public ActionResult<string> Get(string id)
+        {
+            return "Healthy";
+        }
+    }
+}
