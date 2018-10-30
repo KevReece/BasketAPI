c5442270186d48beb6057b77345617f4e78a2a03
KevReece <kevinjamesreece@gmail.com>
2018-10-30 22:27:37 +0100

    Swagger json and UI endpoints

--- src/BasketApi/Startup.cs
+++ src/BasketApi/Startup.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace BasketApi
 {
@@ -9,8 +11,19 @@ namespace BasketApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddSwaggerGen(ConfigureSwaggerGenerationSetup);
         }
-        
+
+        private static void ConfigureSwaggerGenerationSetup(SwaggerGenOptions options)
+        {
+            var info = new Info
+            {
+                Title = "BasketApi",
+                Description = "In memory basket functionality",
+            };  
+            options.SwaggerDoc("v1", info);
+        }
+
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -24,6 +37,8 @@ namespace BasketApi
 
             app.UseMvc();
             app.UseHttpsRedirection();
+
+            app.UseSwagger().UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "BasketApi"));
         }
     }
 }
